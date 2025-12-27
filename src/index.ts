a01c219dda260f8ef75e8c31a27d4b6841081517
Araryarch <araryaarch@gmail.com>
2025-12-27 17:44:02 +0700

    Restore JSON response at root endpoint

--- src/index.ts
+++ src/index.ts
@@ -14,9 +14,27 @@ const app = new Elysia()
         }
     }))
     .use(cors())
-    .get("/", ({ set }) => {
-        set.redirect = "/swagger";
-    })
+    .get("/", () => ({
+        status: "success",
+        message: "Welcome to Samehadaku API",
+        version: "1.0.0",
+        documentation: "/swagger",
+        endpoints: {
+            home: "/anime/samehadaku/home",
+            recent: "/anime/samehadaku/recent?page=1",
+            ongoing: "/anime/samehadaku/ongoing?page=1",
+            completed: "/anime/samehadaku/completed?page=1",
+            popular: "/anime/samehadaku/popular?page=1",
+            movies: "/anime/samehadaku/movies?page=1",
+            search: "/anime/samehadaku/search?query=naruto&page=1",
+            genres: "/anime/samehadaku/genres",
+            genreDetail: "/anime/samehadaku/genres/:genreId?page=1",
+            animeDetail: "/anime/samehadaku/anime/:animeId",
+            episodeDetail: "/anime/samehadaku/episode/:episodeId"
+        },
+        repository: "https://github.com/Araryarch/animify-be",
+        author: "Araryarch"
+    }))
     .use(animeController);
 
 // For Vercel deployment
