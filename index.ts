87204cb3d1f2c894abc9a0adec10fe04c75a4d60
Araryarch <araryaarch@gmail.com>
2025-12-27 17:28:49 +0700

    Add root index.ts and build configuration for Vercel

---
+++ index.ts
@@ -0,0 +1,19 @@
+import { Elysia } from "elysia";
+import { swagger } from "@elysiajs/swagger";
+import { cors } from "@elysiajs/cors";
+import { animeController } from "./src/controllers/anime.controller";
+
+const app = new Elysia()
+    .use(swagger({
+        documentation: {
+            info: {
+                title: 'Samehadaku API',
+                version: '1.0.0',
+                description: 'Unofficial Samehadaku API built with Elysia and Bun'
+            }
+        }
+    }))
+    .use(cors())
+    .use(animeController);
+
+export default app;
