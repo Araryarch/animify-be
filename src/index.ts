fc1b94c45dfc490689f352bae37349c9be1f5893
Araryarch <araryaarch@gmail.com>
2025-12-27 17:42:45 +0700

    Redirect root endpoint to Swagger documentation

--- src/index.ts
+++ src/index.ts
@@ -14,27 +14,9 @@ const app = new Elysia()
         }
     }))
     .use(cors())
-    .get("/", () => ({
-        status: "success",
-        message: "Welcome to Samehadaku API",
-        version: "1.0.0",
-        documentation: "/swagger",
-        endpoints: {
-            home: "/anime/samehadaku/home",
-            recent: "/anime/samehadaku/recent?page=1",
-            ongoing: "/anime/samehadaku/ongoing?page=1",
-            completed: "/anime/samehadaku/completed?page=1",
-            popular: "/anime/samehadaku/popular?page=1",
-            movies: "/anime/samehadaku/movies?page=1",
-            search: "/anime/samehadaku/search?query=naruto&page=1",
-            genres: "/anime/samehadaku/genres",
-            genreDetail: "/anime/samehadaku/genres/:genreId?page=1",
-            animeDetail: "/anime/samehadaku/anime/:animeId",
-            episodeDetail: "/anime/samehadaku/episode/:episodeId"
-        },
-        repository: "https://github.com/Araryarch/animify-be",
-        author: "Araryarch"
-    }))
+    .get("/", ({ set }) => {
+        set.redirect = "/swagger";
+    })
     .use(animeController);
 
 // For Vercel deployment
